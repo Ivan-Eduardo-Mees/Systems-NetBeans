59ed048b7bec30f1ea73bc43a54a38efd6b88814
Ivan-Eduardo-Mees <58777366+Ivan-Eduardo-Mees@users.noreply.github.com>
2019-12-11 10:26:30 -0300

    Initial commit

---
+++ src/systems/Systems.java
@@ -0,0 +1,13 @@
+
+package systems;
+
+
+public class Systems {
+
+    
+    public static void main(String[] args) {
+        TelaPrincipal tp = new TelaPrincipal();
+        tp.setVisible(true);
+    }
+    
+}
